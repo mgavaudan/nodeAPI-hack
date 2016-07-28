cadccc5e50fb2ad116a69f1a1705e5d1405970c6
Matthieu Gavaudan <mlg2199@columbia.edu>
2016-07-28 01:43:20 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// check modulus for user and password
+// le pass est la compagnie + test
+
+module.exports = {
+    'secret': 'test',
+    'database': 'mongodb://user:password@jello.modulusmongo.net:27017/hU4zubap'
+};
